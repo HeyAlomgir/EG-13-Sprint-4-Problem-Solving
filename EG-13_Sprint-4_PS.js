43a92d69c25ce173782695752d3c3e1b9551b9de
Alomgir Hossain <alomgirhosssain71@gmail.com>
2026-09-11 12:12:18 +0600

    feat: solve word pattern problem

--- EG-13_Sprint-4_PS.js
+++ EG-13_Sprint-4_PS.js
@@ -30,4 +30,50 @@ const isIsomorphic = function (s, t) {
 
 
 
-console.log("01. Isomorphic Strings:", isIsomorphic("egg", "add"));
+// console.log("01. Isomorphic Strings:", isIsomorphic("egg", "add"));
+
+
+
+
+
+
+
+// 02. Word Pattern
+
+
+const wordPattern = function (pattern, s) {
+  const words = s.split(" ");
+
+  if (pattern.length !== words.length) {
+    return false;
+  }
+
+  const patternToWord = new Map();
+  const wordToPattern = new Map();
+
+  for (let i = 0; i < pattern.length; i++) {
+    const char = pattern[i];
+    const word = words[i];
+
+    if (
+      (patternToWord.has(char) && patternToWord.get(char) !== word) ||
+      (wordToPattern.has(word) && wordToPattern.get(word) !== char)
+    ) {
+      return false;
+    }
+
+    patternToWord.set(char, word);
+    wordToPattern.set(word, char);
+  }
+
+  return true;
+};
+
+
+// console.log(
+//   "02. Word Pattern:",
+//   wordPattern("abba", "dog cat cat dog")
+// );
+
+
+
